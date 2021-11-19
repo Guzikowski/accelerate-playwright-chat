f1dd031719f692cf6eca9dfb6010c792b49a4971
Ed Guzikowski <ed.guzikowski@ptml.com>
2021-11-19 17:07:08 +1300

    getting local working

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,44 +3,42 @@ import path from 'path';
 
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
-  // Timeout per test
-  timeout: 30 * 1000,
-  // Test directory
-  // testDir: path.join(__dirname, 'e2e'),
-  testDir: path.join(__dirname, 'samples'),
-  // If a test fails, retry it additional 2 times
-  retries: 0,
-  // Artifacts folder where screenshots, videos, and traces are stored.
-  outputDir: 'test-results/',
+	// Timeout per test
+	timeout: 30 * 1000,
+	// Test directory
+	testDir: path.join(__dirname, 'e2e'),
+	// If a test fails, retry it additional 2 times
+	retries: 0,
+	// Artifacts folder where screenshots, videos, and traces are stored.
+	outputDir: 'test-results/',
 
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
+	// Run your local dev server before starting the tests:
+	// https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+	// webServer: {
+	//   command: 'npm run start',
+	//   port: 3000,
+	// },
 
-  use: {
-    headless: false,
-    // Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs, network traffic etc.
-    // More information: https://playwright.dev/docs/trace-viewer
-    trace: 'retry-with-trace',
+	use: {
+		headless: false,
+		// Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs, network traffic etc.
+		// More information: https://playwright.dev/docs/trace-viewer
+		trace: 'retry-with-trace',
 
-    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
-    contextOptions: {
-      ignoreHTTPSErrors: true,
-    },
-  },
+		// All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
+		contextOptions: {
+			ignoreHTTPSErrors: true
+		}
+	},
 
-  projects: [
-    {
-      name: 'Desktop Chrome',
-      use: {
-        ...devices['Desktop Chrome'],
-
-      },
-    },
-    /* {
+	projects: [
+		{
+			name: 'Desktop Chrome',
+			use: {
+				...devices['Desktop Chrome']
+			}
+		}
+		/* {
       name: 'Desktop Firefox',
       use: {
         ...devices['Desktop Firefox'],
@@ -63,6 +61,6 @@ const config: PlaywrightTestConfig = {
       name: 'Mobile Safari',
       use: devices['iPhone 12'],
     }, */
-  ],
+	]
 };
 export default config;
